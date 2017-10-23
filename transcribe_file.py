96322358b1802047e5c45107f0f336ff8488d5ea
Michal Kulinski <michal.kulinski@accenture.com>
2017-10-24 01:05:45 +0200

    python advanced

--- transcribe_file.py
+++ transcribe_file.py
@@ -1,3 +1,6 @@
+import argparse
+import io
+
 def transcribe_file(speech_file):
     """Transcribe the given audio file."""
     from google.cloud import speech
@@ -10,13 +13,16 @@ def transcribe_file(speech_file):
 
     audio = types.RecognitionAudio(content=content)
     config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        encoding=enums.RecognitionConfig.AudioEncoding.MP3,
         sample_rate_hertz=16000,
         language_code='en-US')
 
     response = client.recognize(config, audio)
     # Print the first alternative of all the consecutive results.
+
+    print(response)
+
     for result in response.results:
         print('Transcript: {}'.format(result.alternatives[0].transcript))
 
-transcribe_file()
+transcribe_file('/home/kruszynka/Pobrane/plikiaudio/rozmowa1_ENG/scen1tel1.mp3')
