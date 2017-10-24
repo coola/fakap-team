95000fc2685172819019e88ab923ba76c2a570c1
Michal Kulinski <michal.kulinski@accenture.com>
2017-10-24 22:25:48 +0200

    some works

--- transcribe_file.py
+++ transcribe_file.py
@@ -1,5 +1,7 @@
 import argparse
 import io
+import os
+
 
 def transcribe_file(speech_file):
     """Transcribe the given audio file."""
@@ -8,17 +10,24 @@ def transcribe_file(speech_file):
     from google.cloud.speech import types
     client = speech.SpeechClient()
 
-    from pydub import AudioSegment
-    sound = AudioSegment.from_mp3(speech_file)
-    sound.export("./", format="wav")
+    #temp_file_name = "./export.wav"
+
+    #os.remove(temp_file_name)
+
+    #from pydub import AudioSegment
+    #sound = AudioSegment.from_mp3(speech_file)
+    #"-acodec", "pcm_s16le" ,
+    #sound.export(temp_file_name, format="flac", parameters=["-ac", "1","-ar", "16000" ])
+
+    #os.system("ffmpeg -i #{speech_file} #{temp_file_name}")
 
     with io.open(speech_file, 'rb') as audio_file:
         content = audio_file.read()
 
     audio = types.RecognitionAudio(content=content)
     config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.MP3,
-        sample_rate_hertz=16000,
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
         language_code='en-US')
 
     response = client.recognize(config, audio)
@@ -29,4 +38,4 @@ def transcribe_file(speech_file):
     for result in response.results:
         print('Transcript: {}'.format(result.alternatives[0].transcript))
 
-transcribe_file('/home/kruszynka/Pobrane/plikiaudio/rozmowa1_ENG/scen1tel1.mp3')
+transcribe_file('/home/kruszynka/Pobrane/Source/mastercoder/fakap-team/plikiaudio/rozmowa1_ENG/scen1tel2.flac')
